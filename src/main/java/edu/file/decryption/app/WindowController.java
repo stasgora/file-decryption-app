cdec6a0368ea9032f43ca34b8ad8219e631e37f9
Jan Kruczyński <jan.kruczynski16@gmail.com>
2019-05-01 18:58:24 +0200

    Changed logged user string

--- src/main/java/edu/file/decryption/app/WindowController.java
+++ src/main/java/edu/file/decryption/app/WindowController.java
@@ -58,8 +58,8 @@ public class WindowController {
 
 		successLogin = true; // Just to check if it works
 		if(successLogin) {
-			loggedInLabel.setText(BASE_USER_LOGGED_TEXT + "Tomasz Dziubich");
 			cryptoComponent.loginUser(loginField.getText(), passwordField.getText());
+			loggedInLabel.setText(BASE_USER_LOGGED_TEXT + cryptoComponent.getUserName());
 		}
 	}
 
